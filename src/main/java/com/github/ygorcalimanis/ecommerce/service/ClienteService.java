fabac9162ca97dd90c061afa0ad23f109e5e7fab
ygor-calimanis <ygoralvescalimanis@hotmail.com>
2023-10-10 19:38:03 -0300

    solved bug  deleting tomcat dependency

--- src/main/java/com/github/ygorcalimanis/ecommerce/service/ClienteService.java
+++ src/main/java/com/github/ygorcalimanis/ecommerce/service/ClienteService.java
@@ -2,7 +2,7 @@ package com.github.ygorcalimanis.ecommerce.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.github.ygorcalimanis.ecommerce.model.Cliente;
