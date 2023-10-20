afa83ad41fe29ec0738f8214ea71bf9dc100285c
ygor-calimanis <ygoralvescalimanis@hotmail.com>
2023-10-19 22:12:08 -0300

    Finish

--- src/main/java/com/github/ygorcalimanis/ecommerce/service/ClienteService.java
+++ src/main/java/com/github/ygorcalimanis/ecommerce/service/ClienteService.java
@@ -1,6 +1,7 @@
 package com.github.ygorcalimanis.ecommerce.service;
 
 import java.util.List;
+import java.util.Optional;
 
 // import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,11 +14,19 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class ClienteService {
-	
-	private final ClienteRepository clienteRepository;
-	
-	public List<Cliente> getAll(){
-		return clienteRepository.findAll();
-	} 
-	
+
+    private final ClienteRepository clienteRepository;
+
+    public List<Cliente> getAll() {
+        return clienteRepository.findAll();
+    }
+
+    public boolean exists(long id) {
+        return clienteRepository.existsById(id);
+    }
+
+    public Cliente findById(long id) {
+
+        return clienteRepository.findById(id).orElse(null);
+    }
 }
